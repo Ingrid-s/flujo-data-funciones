9158abf03cb5c9033d9d4cb56c55a0f127a346f3
ingridsag <starlling@hotmail.com>
2018-01-09 16:40:59 -0600

    add students

--- js/app.js
+++ js/app.js
@@ -1,19 +1,31 @@
 var estudiantes = [];
 
 function obtenerListaEstudiantes() {
-    // TO DO: Retornar la lista de estudiantes
-    
-}
+return estudiantes;
+    }
 
 function agregarEstudiante() {
     // TO DO: Preguntar al usuario por el nombre, puntos técnicos y puntos de HSE de un estudiante
     // Hint: El estudiante debe ser agregado a la lista de estudiantes
     // TO DO: Retornar el estudiante recientemente creado
-    
+    var question1 = prompt("¿Cuál es el nombre del estudiante? ");
+    var techPoint = parseInt(prompt("Ingresa los puntos tech"));
+    var hsePoints = parseInt(prompt("Ingresa los puntos HSE"));
+
+    var student = {
+      "nombre": name,
+      "puntos": techPoints,
+      "hse": hsePoints
+    }
+
+
+    estudiantes.push(student);
+    return student;
+
 }
 
 function mostrar(estudiante) {
-    // TO DO: Completar el template para que muestre las propiedades correctas del estudiante 
+    // TO DO: Completar el template para que muestre las propiedades correctas del estudiante
     var resultado = "";
     resultado += "<div class='row'>";
     resultado += "<div class='col m12'>";
@@ -32,22 +44,22 @@ function mostrar(estudiante) {
 function mostrarLista(estudiantes) {
     // TO DO: Iterar la lista del estudiantes para devolverlos en el formato que usa la función mostrar(estudiante)
     // Retornar el template de todos los estudiantes
-    
+
 }
 
 function buscar(nombre, estudiantes) {
     // TO DO: Buscar el nombre en la lista de estudiantes que se recibe por parámetros
     // Retornar el objeto del estudiante buscado
     // Nota: NO IMPORTA SI EL USUARIO ESCRIBE EL NOMBRE EN MAYÚSCULAS O MINÚSCULAS
-    
+
 }
 
 function topTecnico(estudiantes) {
     // TO DO: Retornar el arreglo de estudiantes ordenado por puntaje técnico de mayor a menor
-    
+
 }
 
 function topHSE(estudiantes) {
     // TO DO: Retornar el arreglo de estudiantes ordenado por puntaje de HSE de mayor a menor
-    
+
 }
